f2819b64c5207fcb99031e55c23b31b663408661
Andi <andigeng@users.noreply.github.com>
2016-12-04 02:01:32 -0500

    Add files via upload

--- demo.py
+++ demo.py
@@ -3,29 +3,29 @@ import read_data as rd
 from sample import Sample
 
 def main():
-	
-	# Imports and converts training and test data to useable form
-	training_data = rd.read_data('data/training.txt')
-	rd.hot_encode(training_data)
-	training_data = rd.to_object(training_data)
+  
+  # Imports and converts training and test data to useable form
+  training_data = rd.read_data('data/training.txt')
+  rd.hot_encode(training_data)
+  training_data = rd.to_object(training_data)
 
-	test_data = rd.read_data('data/testing.txt')
-	rd.hot_encode(test_data)
-	test_data = rd.to_object(test_data)
+  test_data = rd.read_data('data/testing.txt')
+  rd.hot_encode(test_data)
+  test_data = rd.to_object(test_data)
 
-	# Initialize neural network
-	net = NeuralNet([64, 90, 10], 0.25, -0.3, 0.3)
+  # Initialize neural network
+  net = NeuralNet([64, 90, 10], 0.25, -0.3, 0.3)
 
-	# Train neural network with 5 epochs
-	net.train_network(training_data, 5)
+  # Train neural network with 5 epochs
+  net.train_network(training_data, 5)
 
-	# Display accuracies for training and testing dataset
-	print('\nFinal Testing Accuracy')
-	print(net.accuracy(test_data))
+  # Display accuracies for training and testing dataset
+  print('\nFinal Testing Accuracy')
+  print(net.accuracy(test_data))
 
-	print('\nFinal Training Accuracy:')
-	print(net.accuracy(training_data))
+  print('\nFinal Training Accuracy:')
+  print(net.accuracy(training_data))
 
 
 if __name__ == "__main__":
-	main()
+  main()
--- sample.py
+++ sample.py
@@ -2,8 +2,8 @@ import numpy as np
 
 class Sample:
 
-	def __init__(self, predictors, target):
-		""" Encapsulation of samples. 
-		"""
-		self.predictors = np.array(predictors)
-		self.target = np.array(target)
+  def __init__(self, predictors, target):
+    """ Encapsulation of samples. 
+    """
+    self.predictors = np.array(predictors)
+    self.target = np.array(target)
